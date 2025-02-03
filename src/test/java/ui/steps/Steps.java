e03e0a13643abb3db63098bb872c9afacf38ba2c
Rzymek <przemek.rzymkowski@gmail.com>
2025-02-03 13:21:33 +0100

    test

--- src/test/java/ui/steps/Steps.java
+++ src/test/java/ui/steps/Steps.java
@@ -22,7 +22,7 @@ public class Steps {
 
     @Given("^Open browser$")
     public void openBrowser() {
-        Configuration.browserSize = "1920x1080";
+        Configuration.browserSize = "2500x1080";
         open("https://www.google.com");
         log.info(Configuration.browserSize);
 
